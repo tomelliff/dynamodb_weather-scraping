3a91ce8a37edfac75ca47e68ba3bffd6d71d285b
Tom Elliff <tomelliff@gmail.com>
2016-02-28 22:07:37 +0000

    Get date of forecast

--- weather_scrape.py
+++ weather_scrape.py
@@ -1,6 +1,7 @@
 import logging
 
 from bs4 import BeautifulSoup
+import dateutil.parser as dparser
 import requests
 
 logging.getLogger().setLevel(logging.INFO)
@@ -18,8 +19,24 @@ def get_forecast_for_postcode(postcode):
 
   return soup
 
+def get_forecast_date(soup):
+  for caption in soup.find_all('caption'):
+    logging.debug(caption.string)
+    if "Weather forecast details for" in caption.string:
+      date_string = caption.string.split('\n', 1)[0]
+
+  logging.info(date_string)
+
+  date = dparser.parse(date_string, fuzzy=True).date()
+
+  logging.info(date)
+
+  return date
+
 def main():
   soup = get_forecast_for_postcode('NN1')
+
+  forecast_date = get_forecast_date(soup)
 
 if __name__ == '__main__':
   main()
