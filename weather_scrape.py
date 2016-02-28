bd655b2bf2b72eedd41b2a218fed2077851a8261
Tom Elliff <tomelliff@gmail.com>
2016-02-28 22:05:27 +0000

    Allow non lowercase postcode entry

--- weather_scrape.py
+++ weather_scrape.py
@@ -3,12 +3,12 @@ import logging
 from bs4 import BeautifulSoup
 import requests
 
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
 
 def get_forecast_for_postcode(postcode):
   BBC_WEATHER_URL = 'http://www.bbc.co.uk/weather/'
 
-  r = requests.get(BBC_WEATHER_URL + postcode)
+  r = requests.get(BBC_WEATHER_URL + postcode.lower())
 
   logging.debug("Status code: {}".format(r.status_code))
 
@@ -19,7 +19,7 @@ def get_forecast_for_postcode(postcode):
   return soup
 
 def main():
-  soup = get_forecast_for_postcode('nn1')
+  soup = get_forecast_for_postcode('NN1')
 
 if __name__ == '__main__':
   main()
